7c61e24b9314fd0a47a655268447da6cb4108ba1
Trey Gordon <41927921+treyg@users.noreply.github.com>
2023-07-20 15:31:05 -0400

    chore: remove old config

--- config.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  sendBy: 'day', //options are 'day', 'week' or 'month'
-  sendEmail: 'gordont21@gmail.com',
-  recieveEmail: 'tag@treyg.dev',
-  emailSubject: 'Your Daily Photos'
-}
-
-export default config
